627edeefa27ea27c6e73c56add44d3c2f73c08fb
simonevazzana <simo.vazzana@gmail.com>
2023-09-19 14:39:46 +0200

    feat: added commands, skeleton for scraping data related to the board game

---
+++ commands/scrape-board.js
@@ -0,0 +1,45 @@
+const { parseStartSeason, parseEndSeason } = require('../lib/utils/parse-input-data')
+
+module.exports = {
+  command: 'scrape-board',
+  desc: 'Scrapes all leagues until a given season, saving all data for the board game',
+  builder: {
+    startSeason: {
+      required: true,
+      alias: 's',
+      description: 'Oldest season',
+      coerce: parseStartSeason
+    },
+    endSeason: {
+      required: false,
+      alias: 'e',
+      description: 'Latest season',
+      default: 2023,
+      coerce: parseEndSeason
+    }
+  },
+  async handler ({ startSeason, endSeason }) {
+    const leagues = [
+      'Bundesliga',
+      'Eredivisie',
+      'La Liga',
+      'Liga Portugal',
+      'Ligue 1',
+      'Premier League',
+      'Serie A'
+    ]
+
+    if (startSeason > endSeason) throw new Error('startSeason can\'t be higher than endSeason')
+
+    const seasons = (Array.from(Array(endSeason - startSeason).keys())).map(v => v + startSeason).reverse()
+
+    for (const season of seasons) {
+      for (const league of leagues) {
+        console.log(`Started scraping league: ${league}, season: ${season}/${season + 1}`)
+        // Where the actual scraping will happen
+        console.log(`Finished scraping all teams from league: ${league}, season: ${season}/${season + 1}`)
+      }
+    }
+    process.exit(0)
+  }
+}
